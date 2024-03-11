b37cfcdad2f904acc1a559595ea1982f728118a5
dyoshyy <d.yoshhyy@gmail.com>
2024-03-12 01:20:25 +0900

    update: promptの修正

--- src/controller/weeklyReport/weekly_report_controller.ts
+++ src/controller/weeklyReport/weekly_report_controller.ts
@@ -6,29 +6,36 @@ import { WeeklyReport } from "../../model/weeklyReport/types";
 import { listWeeklyReportsService } from "../../service/weeklyReport/list_weekly_reports_service";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import "dotenv/config";
+import { kMaxLength } from "buffer";
 
-const runGemini = async (weeklyReportsList: { weeklyReports: WeeklyReport[] }) => {
-  const genAI = new GoogleGenerativeAI(process.env.GENERATIVE_AI_API_KEY as string);
+const runGemini = async (weeklyReports: WeeklyReport[]) => {
+  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const latestIndex = weeklyReports.length - 1;
   const prompt = `
     # 目的
-    以下のデータはルーティンの実施状況をまとめた週次レポートです。
-    先週の結果を踏まえて、今週の結果のいいところ、悪いところをまとめ、次週の目標を立ててください。
+    あなたはルーティン化達成アドバイザーです。
+    先週と今週の達成状況を比較し、良い点、悪い点について５０文字程度でアドバイスしてください。
+    また、返答にはマークダウン記法や改行は使用しないでください。
 
     # 週次レポート
     ## 先週の結果
-    - 完了したルーティン数: ${weeklyReportsList.weeklyReports[0].completedQuests}
-    - 失敗したルーティン数: ${weeklyReportsList.weeklyReports[0].failedQuests}
-    - 完了率: ${weeklyReportsList.weeklyReports[0].completedPercentage}%
-    - 完了日数: ${weeklyReportsList.weeklyReports[0].completedDays}
-    - 各日の完了ルーティン数: ${weeklyReportsList.weeklyReports[0].completedQuestsEachDay}
+    - 完了したルーティン数: ${weeklyReports[latestIndex - 1].completedQuests}
+    - 失敗したルーティン数: ${weeklyReports[latestIndex - 1].failedQuests}
+    - 完了率: ${weeklyReports[latestIndex - 1].completedPercentage}%
+    - 完了日数: ${weeklyReports[latestIndex - 1].completedDays}
+    - 各日の完了ルーティン数: ${weeklyReports[latestIndex - 1].completedQuestsEachDay}
+    // - 開始日: ${weeklyReports[latestIndex - 1].startDate}
+    // - 終了日: ${weeklyReports[latestIndex - 1].endDate}
 
     ## 今週の結果
-    - 完了したルーティン数: ${weeklyReportsList.weeklyReports[1].completedQuests}
-    - 失敗したルーティン数: ${weeklyReportsList.weeklyReports[1].failedQuests}
-    - 完了率: ${weeklyReportsList.weeklyReports[1].completedPercentage}%
-    - 完了日数: ${weeklyReportsList.weeklyReports[1].completedDays}
-    - 各日の完了ルーティン数: ${weeklyReportsList.weeklyReports[1].completedQuestsEachDay}
+    - 完了したルーティン数: ${weeklyReports[latestIndex].completedQuests}
+    - 失敗したルーティン数: ${weeklyReports[latestIndex].failedQuests}
+    - 完了率: ${weeklyReports[latestIndex].completedPercentage}%
+    - 完了日数: ${weeklyReports[latestIndex].completedDays}
+    - 各日の完了ルーティン数: ${weeklyReports[latestIndex].completedQuestsEachDay}
+    // - 開始日: ${weeklyReports[latestIndex].startDate}
+    // - 終了日: ${weeklyReports[latestIndex].endDate}
   `;
   const result = await model.generateContent(prompt);
   const response = await result.response;
@@ -72,7 +79,7 @@ export const summarizeWeeklyReportController = async (req: Request, res: Respons
 
   try {
     const weeklyReportsList = await listWeeklyReportsService({ userId: userId });
-    const summary = await runGemini(weeklyReportsList);
+    const summary = await runGemini(weeklyReportsList.weeklyReports);
 
     const response: SummarizeWeeklyReportResponse = {
       status: "ok",
