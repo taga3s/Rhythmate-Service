6c52390fec5c3ddfccfbfe4a21c4ebdf7de66cbf
dyoshyy <d.yoshhyy@gmail.com>
2024-03-13 22:47:33 +0900

    fix: routesの修正

--- src/route/weeklyReport.ts
+++ src/route/weeklyReport.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import {
   listWeeklyReportController,
-  summarizeWeeklyReportController,
+  getWeeklyReportSummaryController,
 } from "../controller/weeklyReport/weekly_report_controller";
 import { auth } from "./middlewares/auth";
 
 const weeklyReportRouter = Router();
 
 weeklyReportRouter.get("/", auth, listWeeklyReportController);
-weeklyReportRouter.get("/summarize", auth, summarizeWeeklyReportController);
+weeklyReportRouter.get("/summarize", auth, getWeeklyReportSummaryController);
 
 export { weeklyReportRouter };
