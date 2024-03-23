65643269dc7f9f9ecc3807acff50e846cba8cbb0
dyoshyy <d.yoshhyy@gmail.com>
2024-03-24 00:45:54 +0900

    fix: 終了した週に対してのみsummaryを生成するように修正

--- src/service/weeklyReport/get_weekly_report_summary_service.ts
+++ src/service/weeklyReport/get_weekly_report_summary_service.ts
@@ -3,6 +3,7 @@ import { WeeklyReportModel } from "../../model/weeklyReport/weekly_report_model"
 import { HttpError } from "../../pkg/httpError";
 import { WeeklyReport } from "@prisma/client";
 import "dotenv/config";
+import { isBefore } from "../../pkg/dayjs";
 
 const runGemini = async (weeklyReports: WeeklyReport[]) => {
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
@@ -48,20 +49,27 @@ export const getWeeklyReportSummaryService = async (inputDTO: {
   const thisWeekReport = weeklyReports[inputDTO.weeklyReportIndex]; // 今週の週次レポート
   const lastWeekReport = weeklyReports[inputDTO.weeklyReportIndex + 1]; // 先週の週次レポート
 
-  // summaryが空文字でない場合はそれを返す
-  if (thisWeekReport.summary !== "") {
-    return thisWeekReport.summary;
+  // 週の途中の場合
+  if (!isBefore(thisWeekReport.endDate)) {
+    return "一週間が終了するとアドバイスが生成されます!";
   }
-  // 最初の週の場合
-  else if (lastWeekReport === undefined) {
-    const firstSummary = "最初の１週間お疲れさまでした！来週以降アドバイスが生成されます！";
-    await model.saveSummary(thisWeekReport.id, firstSummary);
-    return firstSummary;
-  }
-  // summaryが空文字の場合は要約を生成し、DBに保存する
+  // 終了した週の場合
   else {
-    const summary = await runGemini([thisWeekReport, lastWeekReport]);
-    await model.saveSummary(thisWeekReport.id, summary);
-    return summary;
+    // summaryが空文字でない場合はそれを返す
+    if (thisWeekReport.summary !== "") {
+      return thisWeekReport.summary;
+    }
+    // 最初の週の場合
+    else if (lastWeekReport === undefined) {
+      const firstSummary = "最初の１週間お疲れさまでした!来週も頑張りましょう!";
+      await model.saveSummary(thisWeekReport.id, firstSummary);
+      return firstSummary;
+    }
+    // summaryが空文字の場合は要約を生成し、DBに保存する
+    else {
+      const summary = await runGemini([thisWeekReport, lastWeekReport]);
+      await model.saveSummary(thisWeekReport.id, summary);
+      return summary;
+    }
   }
 };
