69a76b2568f483ef255cd613febf52e966ad25a3
dyoshyy <d.yoshhyy@gmail.com>
2024-03-30 00:00:42 +0900

    update: table名の変更

---
+++ prisma/migrations/20240329150013_/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `BadgeList` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "BadgeList";
+
+-- CreateTable
+CREATE TABLE "BadgeDetail" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageDir" TEXT NOT NULL,
+
+    CONSTRAINT "BadgeDetail_pkey" PRIMARY KEY ("id")
+);
