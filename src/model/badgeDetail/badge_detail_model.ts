00dbd323ff1af7a999a5b0cc67925dc2492bf80b
dyoshyy <d.yoshhyy@gmail.com>
2024-03-30 00:07:51 +0900

    feat: badge detail modelの実装

---
+++ src/model/badgeDetail/badge_detail_model.ts
@@ -0,0 +1,56 @@
+import { Prisma } from "@prisma/client";
+import { PrismaClientWithTx } from "../../db/types";
+import { BadgeDetail } from "./type";
+import { prisma } from "../../db/db";
+
+export class BadgeDetailModel {
+  public async createWithTx(
+    name: string,
+    description: string,
+    imageDir: string,
+    tx: PrismaClientWithTx
+  ): Promise<BadgeDetail> {
+    const badgeDetail: Prisma.BadgeDetailCreateInput = {
+      name: name,
+      description: description,
+      imageDir: imageDir,
+    };
+    const result = await tx.badgeDetail.create({ data: badgeDetail });
+    return result;
+  }
+
+  public async updateWithTx(
+    id: string,
+    name: string,
+    description: string,
+    imageDir: string,
+    tx: PrismaClientWithTx
+  ): Promise<BadgeDetail | null> {
+    const badgeDetail: Prisma.BadgeDetailUpdateInput = {
+      name: name,
+      description: description,
+      imageDir: imageDir,
+    };
+    const result = await tx.badgeDetail.update({
+      where: { id: id },
+      data: badgeDetail,
+    });
+    return result;
+  }
+
+  public async deleteWithTx(id: string, tx: PrismaClientWithTx): Promise<BadgeDetail | null> {
+    const result = await tx.badgeDetail.delete({
+      where: { id: id },
+    });
+    return result;
+  }
+
+  public async getById(id: string): Promise<BadgeDetail | null> {
+    const result = await prisma.badgeDetail.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    return result;
+  }
+}
---
+++ src/model/badgeDetail/type.ts
@@ -0,0 +1,6 @@
+export type BadgeDetail = {
+  id : string;
+  name : string;
+  description : string;
+  imageDir : string;
+  }
