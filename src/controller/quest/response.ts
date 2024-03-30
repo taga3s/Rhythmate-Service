ac106351e8fee64735eee015d87b8964daaca93d
seiya_tagami <bepozurapu274@gmail.com>
2024-03-30 14:31:54 +0900

    fix: クエストのレスポンスの型を修正

--- src/controller/quest/response.ts
+++ src/controller/quest/response.ts
@@ -1,40 +1,9 @@
-export type CreateQuestResponse = {
+export type CreateQuestResponse = QuestBaseResponse & {
   status: string;
-  id: string;
-  title: string;
-  description: string;
-  starts_at: string;
-  started_at: string;
-  minutes: number;
-  tag_id: string;
-  difficulty: string;
-  state: string;
-  is_succeeded: boolean;
-  start_date: string;
-  end_date: string;
-  days: string[];
-  weekly_frequency: number;
 };
 
-export type UpdateQuestResponse = {
+export type UpdateQuestResponse = QuestBaseResponse & {
   status: string;
-  id: string;
-  title: string;
-  description: string;
-  starts_at: string;
-  started_at: string;
-  minutes: number;
-  tag_id: string;
-  difficulty: string;
-  state: string;
-  is_succeeded: boolean;
-  continuation_level: number;
-  start_date: string;
-  end_date: string;
-  days: string[];
-  weekly_frequency: number;
-  weekly_completion_count: number;
-  total_completion_count: number;
 };
 
 export type DeleteQuestResponse = {
@@ -43,29 +12,22 @@ export type DeleteQuestResponse = {
 
 export type ListQuestsResponse = {
   status: string;
-  quests: {
-    id: string;
-    title: string;
-    description: string;
-    starts_at: string;
-    started_at: string;
-    minutes: number;
-    tag_id: string;
-    difficulty: string;
-    state: string;
-    is_succeeded: boolean;
-    continuation_level: number;
-    start_date: string;
-    end_date: string;
-    days: string[];
-    weekly_frequency: number;
-    weekly_completion_count: number;
-    total_completion_count: number;
-  }[];
+  quests: QuestBaseResponse[];
 };
 
-export type StartQuestResponse = {
+export type StartQuestResponse = QuestBaseResponse & {
   status: string;
+};
+
+export type FinishQuestResponse = QuestBaseResponse & {
+  status: string;
+};
+
+export type ForceFinishQuestResponse = QuestBaseResponse & {
+  status: string;
+};
+
+type QuestBaseResponse = {
   id: string;
   title: string;
   description: string;
@@ -76,53 +38,45 @@ export type StartQuestResponse = {
   difficulty: string;
   state: string;
   is_succeeded: boolean;
-  continuation_level: number;
-  start_date: string;
-  end_date: string;
   days: string[];
+  continuation_level: number;
   weekly_frequency: number;
   weekly_completion_count: number;
   total_completion_count: number;
 };
 
-export type FinishQuestResponse = {
-  status: string;
+export const toQuestBaseResponse = (obj: {
   id: string;
   title: string;
   description: string;
-  starts_at: string;
-  started_at: string;
+  startsAt: string;
+  startedAt: string;
   minutes: number;
-  tag_id: string;
+  tagId: string;
   difficulty: string;
   state: string;
-  is_succeeded: boolean;
-  continuation_level: number;
-  start_date: string;
-  end_date: string;
+  isSucceeded: boolean;
+  continuationLevel: number;
   days: string[];
-  weekly_frequency: number;
-  weekly_completion_count: number;
-  total_completion_count: number;
-};
-
-export type ForceFinishQuestResponse = {
-  status: string;
-  id: string;
-  title: string;
-  description: string;
-  starts_at: string;
-  started_at: string;
-  minutes: number;
-  tag_id: string;
-  difficulty: string;
-  state: string;
-  is_succeeded: boolean;
-  continuation_level: number;
-  start_date: string;
-  end_date: string;
-  days: string[];
-  weekly_frequency: number;
-  weekly_completion_count: number;
-  total_completion_count: number;
+  weeklyFrequency: number;
+  weeklyCompletionCount: number;
+  totalCompletionCount: number;
+}): QuestBaseResponse => {
+  return {
+    id: obj.id,
+    title: obj.title,
+    description: obj.description,
+    starts_at: obj.startsAt,
+    started_at: obj.startedAt,
+    minutes: obj.minutes,
+    tag_id: obj.tagId,
+    difficulty: obj.difficulty,
+    state: obj.state,
+    is_succeeded: obj.isSucceeded,
+    continuation_level: obj.continuationLevel,
+    days: obj.days,
+    weekly_frequency: obj.weeklyFrequency,
+    weekly_completion_count: obj.weeklyCompletionCount,
+    total_completion_count: obj.totalCompletionCount,
+  };
 };
