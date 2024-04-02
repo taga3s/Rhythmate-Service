90cfb4ebdfab5b5f033ca3d81f158c747897b282
dyoshyy <d.yoshhyy@gmail.com>
2024-04-02 22:55:42 +0900

    fix: 交差

--- src/model/badge/types.ts
+++ src/model/badge/types.ts
@@ -8,15 +8,8 @@ export type Badge = {
   userId: string;
 };
 
-export type BadgeWithDetail = {
-  id: string;
-  badgeId: string;
+export type BadgeWithDetail = Badge & {
   name: string;
   description: string;
   imageType: string;
-  obtainedAt: string;
-  isPinned: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-  userId: string;
 };
