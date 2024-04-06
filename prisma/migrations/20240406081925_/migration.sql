a42b3a6a74487cc45876d3a709740a0a8c0a743a
dyoshyy <d.yoshhyy@gmail.com>
2024-04-06 17:20:20 +0900

    fix: WeeklyReportテーブルからsummaryを削除

---
+++ prisma/migrations/20240406081925_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `summary` on the `WeeklyReport` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "WeeklyReport" DROP COLUMN "summary";
